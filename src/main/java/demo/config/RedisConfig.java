6ce52ccaf5525a0651aab05d4ff507f7ccb66413
wenzufan <wenzufan@localhost>
2019-12-26 15:57:59 +0800

    打开Redis事务支持

--- src/main/java/demo/config/RedisConfig.java
+++ src/main/java/demo/config/RedisConfig.java
@@ -19,6 +19,7 @@ public class RedisConfig {
 	public  RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory){
 		RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
 		template.setConnectionFactory(factory);
+		template.setEnableTransactionSupport(true);
 		Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
 		ObjectMapper om = new ObjectMapper();
 		om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
