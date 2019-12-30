c9157984ae88ac818582767c699c8ebb2d030d80
Dobby <41136354+Dobby312@users.noreply.github.com>
2019-12-30 10:01:59 +0800

    Add files via upload

---
+++ CronOS/src/main/java/com/example/demo/CronOsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CronOsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CronOsApplication.class, args);
+	}
+
+}
---
+++ CronOS/src/main/java/com/example/demo/util/AsyncScheduleTask.java
@@ -0,0 +1,36 @@
+/**
+ * @author Dobby
+ * Dec 30, 2019
+ */
+package com.example.demo.util;
+
+import java.time.LocalDateTime;
+
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Dobby
+ *
+ * Dec 30, 2019
+ */
+@Component
+@EnableScheduling  
+@EnableAsync  //开启多线程任务
+public class AsyncScheduleTask {
+
+	@Async
+	@Scheduled(fixedRate=2000)
+	public void AsyncTask() throws InterruptedException {
+		System.out.println("第一个任务 ："+ LocalDateTime.now().toLocalTime() + "\r\n线程 : " + Thread.currentThread().getName());
+	}
+	
+	@Async
+	@Scheduled(fixedRate=3000)
+	public void AsyncTask2() throws InterruptedException {
+		System.out.println("第二个定时任务开始 : " + LocalDateTime.now().toLocalTime() + "\r\n线程 : " + Thread.currentThread().getName());
+	}
+}
